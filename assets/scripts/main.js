fc13af54c3aafda15eeecb95a37594980f7e50e9
beef <mark@benewith.com>
2017-12-05 11:04:16 +0000

    added collapse js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,7 +11,7 @@ import './ajax-form';
 // import 'bootstrap/js/dist/alert.js';
 // import 'bootstrap/js/dist/button.js';
 // import 'bootstrap/js/dist/carousel.js';
-// import 'bootstrap/js/dist/collapse.js';
+import 'bootstrap/js/dist/collapse.js';
 // import 'bootstrap/js/dist/dropdown.js';
 // import 'bootstrap/js/dist/modal.js';
 // import 'bootstrap/js/dist/popover.js';
