2e846ee06395d824969d79fdefcdec6b282ee787
Mark Benewith <mark@benewith.com>
2019-12-04 12:31:10 +0000

    add newsletter in footer

--- footer.php
+++ footer.php
@@ -7,8 +7,8 @@ if (get_theme_mod('footer_sticky') === 'enabled') : ?>
 
 <footer>
   <div class="container">
-    <div class="row">
-      <div class="col-12 col-lg-2 text-center">
+    <div class="row justify-content-between">
+      <div class="col-12 col-lg-6 text-center text-lg-left">
         <a class="navbar-brand" href="<?php echo home_url(); ?>" title="<?php echo esc_attr(bloginfo('name')); ?>">
           <?php if (get_field('logo', 'option')) : ?>
             <?php $img = get_field('logo', 'option'); ?>
@@ -17,6 +17,11 @@ if (get_theme_mod('footer_sticky') === 'enabled') : ?>
             <?php echo bloginfo('name'); ?>
           <?php endif; ?>
         </a>
+      </div>
+      <div class="col-12 col-lg-6 text-center text-lg-right">
+        <?php if(get_field('mailchimp_embed_active', 'option')) { ?>
+          <?php get_template_part('/templates/partials/mailchimp-footer'); ?>
+        <?php } ?>
       </div>
     </div>
     <div class="text-center">
---
+++ templates/partials/mailchimp-footer.php
@@ -0,0 +1,25 @@
+<?php $url = get_field('mailchimp_embed_form_action_code', 'option'); ?>
+
+<div id="mc_embed_signup">
+  <form action="<?php echo $url; ?>" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" validate>
+    <div id="mc_embed_signup_scroll">
+      <h2>Subscribe to our newsletter</h2>
+      <div class="mc-field-group">
+        <div class="wrap">
+          <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email address">
+          <input type="submit" value="Go" name="subscribe" id="mc-embedded-subscribe" class="button">
+        </div>
+      </div>
+      <div id="mce-responses" class="clear">
+        <div class="response" id="mce-error-response" style="display:none"></div>
+        <div class="response" id="mce-success-response" style="display:none"></div>
+      </div>  
+      <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+      <div style="position: absolute; left: -5000px;" aria-hidden="true">
+        <input type="text" name="b_abee93598d8aedb981edc40ef_518fdb0b4e" tabindex="-1" value="">
+      </div>
+    </div>
+  </form>
+</div>
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+<!--End mc_embed_signup-->
