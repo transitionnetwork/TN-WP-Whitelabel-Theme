d144079b65322fc61f3fb9099eaacc26ffb4476c
Mark Benewith <mark@benewith.com>
2021-07-20 15:54:48 +0100

    fix comments issue with comments displaying on every page

--- comments.php
+++ comments.php
@@ -1,12 +1,13 @@
 <?php
 //Get only the approved comments
 $args = array(
-  'status' => 'approve'
+  'status' => 'approve',
+  'post_id' => $post->ID
 );
  
 // The comment Query
-$comments_query = new WP_Comment_Query;
-$comments = $comments_query->query( $args );
+$comments_query = new WP_Comment_Query($args);
+$comments = $comments_query->comments;
  
 // Comment Loop
 if ( $comments ) { ?>
